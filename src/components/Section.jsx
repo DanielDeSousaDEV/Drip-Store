c4c54fade3ddbc7bea1ad712d64de3b3e93f0bef
DanielDeSousaDEV <danieldesousa.dev@gmail.com>
2025-06-15 20:50:30 -0300

    Making the the reusable components

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,6 +1,6 @@
-export function Section({link, titleAlign='left', children}) {
+export function Section({link, titleAlign='left', className, children}) {
     return (
-        <div>
+        <div className={ className ?? ''}>
             <p className="mb-5" style={{textAlign: titleAlign}}>
                 <a href={link.href} className="text-dark-gray-2 text-2xl font-semibold capitalize">{link.title}</a>
             </p>
